9afdbb7b975adf019d8f5067c204c349bbf456a4
aevins <aevins@gmail.com>
2025-11-11 11:15:23 -0500

    Refactor callbacks for training: separate low loss and high accuracy conditions. Added Keras 3 compatibility

--- chapter2/fashion-mnist-withcallbacks.py
+++ chapter2/fashion-mnist-withcallbacks.py
@@ -9,23 +9,38 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# AE - Modified imports to support Keras 3
 import tensorflow as tf
+import keras
 
-class myCallback(tf.keras.callbacks.Callback):
-  def on_epoch_end(self, epoch, logs={}):
-    if(logs.get('accuracy')>0.95):
-      print("\nReached 95% accuracy so cancelling training!")
-      self.model.stop_training = True
+class CallbackLowLoss(keras.callbacks.Callback):
+    """
+    Callback to stop training when loss is low enough.
+    """
+    def on_epoch_end(self, epoch, logs=None):
+        if logs.get('loss') < 0.4:
+            print("\nLoss is low so cancelling training!")
+            self.model.stop_training = True
 
-callbacks = myCallback()
-mnist = tf.keras.datasets.fashion_mnist
+class CallbackHighAccuracy(keras.callbacks.Callback):
+    """
+    Callback to stop training when accuracy is high enough.
+    """
+    def on_epoch_end(self, epoch, logs=None):
+        if logs.get('accuracy') > 0.95:
+            print("\nReached 95% accuracy so cancelling training!")
+            self.model.stop_training = True
+
+callbacks = [CallbackHighAccuracy(), CallbackLowLoss()]
+mnist = keras.datasets.fashion_mnist
 (training_images, training_labels), (test_images, test_labels) = mnist.load_data()
 training_images=training_images/255.0
 test_images=test_images/255.0
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(128, activation=tf.nn.relu),
-  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+model = keras.models.Sequential([
+    keras.layers.Flatten(),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(training_images, training_labels, epochs=50, callbacks=[callbacks])
+model.fit(training_images, training_labels, epochs=50, callbacks=callbacks)
